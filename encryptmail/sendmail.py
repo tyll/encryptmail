0638ae0740a8c41f8bd1f102552d2e3e5f6865c5
Till Maas <opensource@till.name>
2015-11-17 18:25:29 +0100

    Use argparse for sendmail and handle -oi

--- encryptmail/sendmail.py
+++ encryptmail/sendmail.py
@@ -18,6 +18,7 @@
 #    along with encryptmail.  If not, see <http://www.gnu.org/licenses/>.
 # }}}
 
+import argparse
 from email.parser import Parser
 import email.utils
 import os
@@ -40,25 +41,27 @@ def connect_and_send(sockaddr, message):
 
 
 def sendmail(sockaddr):
-    read_message = False
-    recipients = []
-    for arg in sys.argv[1:]:
-        if arg == "-t":
-            read_message = True
-        elif arg == "--":
-            pass
-        else:
-            recipients.append(arg)
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-t", dest="read_message", default=False,
+                           action="store_true",
+                           help="Add recipients from message headers to "
+                                "recipients specified on commandline")
+    argparser.add_argument("-oi", dest="until_dotline",
+                           help="Do not treat line consisting of only a "
+                           "dot ('.') as end of message",
+                           action="store_false", default=True)
+    argparser.add_argument("recipients", nargs="*")
+    args = argparser.parse_args()
 
     raw_message = ""
     while True:
         line = sys.stdin.readline()
         # read until EOF or until a line containing a single dot is read
-        if not line or line == ".\n":
+        if not line or (args.until_dotline and line == ".\n"):
             break
         raw_message += line
 
-    if read_message:
+    if args.read_message:
         parser = Parser()
         message = parser.parsestr(raw_message)
         for header in "To", "Cc", "Bcc":
@@ -68,12 +71,13 @@ def sendmail(sockaddr):
 
             raw_addresses = [x[1] for x in email.utils.getaddresses(
                 other_recipients)]
-            recipients.extend(raw_addresses)
+            args.recipients.extend(raw_addresses)
     user = os.environ.get("USER", "unknown")
     hostname = os.environ.get("HOSTNAME", "localhost")
     fromaddr = user + "@" + hostname
 
-    mail = Mail(fromaddr=fromaddr, recipients=recipients, message=raw_message)
+    mail = Mail(fromaddr=fromaddr, recipients=args.recipients,
+                message=raw_message)
     success = connect_and_send(sockaddr, mail.json)
     if success:
         return 0
